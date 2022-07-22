cc179e97620e7d9a02c16497dd275ebcc2217a6c
Alaikum <carangiadamo@gmail.com>
2022-07-22 16:36:37 +0200

    FizzBuzz Buzz and Fizz

--- js/myscript.js
+++ js/myscript.js
@@ -3,9 +3,18 @@
 
 // CONSEGNA:
 // Scrivi un programma che stampi in console i numeri da 1 a 100,
+
+
+
 //     ma che per i multipli di 3 stampi “Fizz” al posto del numero
+
+
 // e per i multipli di 5 stampi “Buzz”.
+
+
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Come faccio a sapere se un numero è divisibile per un altro ?
 //  Abbiamo visto qualcosa di particolare che possiamo usare ?
@@ -21,6 +30,23 @@
 
 
 
-for(let i=0; i<100; i++){
-    console.log(i +1);  
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        console.log('FizzBuzz');
+
+    } else if (i % 5 === 0) {
+
+        console.log("Buzz")
+
+    } else if (i % 3 === 0) {
+
+        console.log("Fizz")
+
+    } else {
+
+        console.log(i);
+
+    }
 }
